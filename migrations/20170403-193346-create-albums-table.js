3b859b03e9b045a9aa353d579c9ca8222311b946
Patrick Spek <p.spek@tyil.nl>
2017-04-03 19:42:21 +0200

    Add migration for albums

---
+++ migrations/20170403-193346-create-albums-table.js
@@ -0,0 +1,36 @@
+const db = require("../app/models/index.js");
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable("albums", {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+      },
+      artistId: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: "artists",
+          key: "id"
+        }
+      },
+      mbid: {
+        type: Sequelize.UUID
+      },
+      wanted: {
+        type: Sequelize.BOOLEAN
+      },
+      updatedAt: {
+        type: Sequelize.DATE
+      },
+      createdAt: {
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable("albums");
+  }
+};
+
